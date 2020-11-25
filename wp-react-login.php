feab4d2ffab1c075ee0a15ddeb64cde228ce3220
popeating <pop@popeating.it>
2020-11-25 21:16:53 +0100

    added login php

---
+++ wp-react-login.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once('wp-load.php');
+
+$response = array(
+    'data'        => array(),
+    'msg'        => 'Invalid email or password',
+    'status'    => false
+);
+
+
+/* Sanitize all received posts */
+foreach ($_POST as $k => $value) {
+    $_POST[$k] = sanitize_text_field($value);
+}
+header('Content-type: application/json');
+
+/**
+ * Login Method
+ *
+ */
+if (isset($_POST['type']) &&  $_POST['type'] == 'login') {
+
+    /* Get user data */
+    $user = get_user_by('email', $_POST['email']);
+    if ($user) {
+
+
+        $password_check = wp_check_password($_POST['password'], $user->user_pass, $user->ID);
+        if ($password_check) {
+            update_user_meta($user->ID, 'push_token', $_POST['push_token']);
+            /* Generate a unique auth token */
+            $token = md5(uniqid());
+            /* Store / Update auth token in the database */
+            if (update_user_meta($user->ID, 'auth_token', $token)) {
+                $response['avatar'] = get_avatar_url($user->ID);
+                /* Return generated token and user ID*/
+                $response['status'] = true;
+                $response['data'] = array(
+                    'auth_token'     =>    $token,
+                    'user_id'        =>    $user->ID,
+                    'user_login'    =>    $user->user_login
+                );
+                $response['msg'] = 'Successfully Authenticated';
+                $response['token'] = $token;
+            }
+        }
+    }
+}
+echo json_encode($response);
